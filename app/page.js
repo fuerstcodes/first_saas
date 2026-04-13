6769b0e640c1dc4bfae6a57ae76b79351e6d4235
David Fuerst <david.fuerst23@gmail.com>
2026-04-13 22:05:40 +0200

    fix responsiveness in the navbar

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto bg-base-200 flex justify-between items-center px-8 py-2">
           <div className="font-bold">My first SAAS</div>
-          <div className="space-x-4">
+          <div className="space-x-4 max-md:hidden">
             <a className="link link-hover">Pricing</a>
             <a className="link link-hover">FAQ</a>
           </div>
@@ -20,7 +20,7 @@ export default function Home() {
       </section>
 
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-6">
+        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect customer feedback to build better products
         </h1>
         <div className="opacity-90 mb-10">
